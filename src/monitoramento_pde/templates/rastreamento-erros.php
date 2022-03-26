f61b138c54611d23bdda3c39b83c3b9ddc969cf5
vgiuliani11 <vinicius.giuliani@gmail.com>
2022-03-25 21:27:41 -0300

    Otimização da atribuição dos nomes dos indicadores (issue #74)

--- src/monitoramento_pde/templates/rastreamento-erros.php
+++ src/monitoramento_pde/templates/rastreamento-erros.php
@@ -66,19 +66,60 @@ if ($autorizado) {
 			$scope.tabAtivaProblemas = 1;
 			$scope.carregandoProblemas = true;
 			$scope.problemas = <?=$problemas?>;
-			$scope.problemas.carregados = 0;
-			// Recupera nomes dos indicadores
-			$scope.problemas.forEach(obj => {
-				Indicador.query({indicador: obj.id_indicador}, (indicador) => {
-					obj.nome_indicador = indicador[0].nome;
-					$scope.problemas.carregados++;
-					if ($scope.problemas.carregados == $scope.problemas.length) {
-						$scope.carregandoProblemas = false;
-					}
-				});
-			});
+			$scope.arrayNomes = [];
 			
 			Menu.get({id: 7}, menu => $scope.menuProblemas = menu);
+			
+			// Atribuição dos nomes aos indicadores
+			$scope.resgatarNomes = estado => {
+				let arrayIds = [];
+				const filtroProblemas = $scope.problemas.filter(obj => {
+					return obj.resolvido == estado;
+				});
+				filtroProblemas.forEach(obj => {
+					arrayIds.push(parseInt(obj.id_indicador));
+				});
+				arrayIds = [...new Set(arrayIds)];
+
+				if ($scope.arrayNomes.length > 0) {
+					let arrayIdsAntigos = [];
+					$scope.arrayNomes.forEach(obj => {
+						if (!arrayIdsAntigos.includes(obj.id_indicador)) {
+							arrayIdsAntigos.push(obj.id_indicador);
+						}
+					});
+					arrayIds = arrayIds.filter(ele => {
+						return !arrayIdsAntigos.includes(ele)
+					});
+				}
+
+				if (arrayIds.length > 0) {
+					$scope.carregandoProblemas = true;
+					const contagemNomes = arrayIds.length + $scope.arrayNomes.length;
+					arrayIds.forEach(ele => {
+						Indicador.query({indicador: ele}, indicador =>{
+							$scope.arrayNomes.push({
+								id_indicador: indicador[0].id_indicador,
+								nome_indicador: indicador[0].nome
+							});
+							if ($scope.arrayNomes.length == contagemNomes) {
+								$scope.atribuirNomes();
+							}
+						})
+					});
+				}
+			};
+
+			$scope.atribuirNomes = () => {
+				$scope.problemas.forEach(problema => {
+					$scope.arrayNomes.forEach(nomes => {
+						if (problema.id_indicador == nomes.id_indicador) {
+							problema.nome_indicador = nomes.nome_indicador;
+						}
+					});
+				});
+				$scope.carregandoProblemas = false;
+			}
 
 			// Execução de funções ao abrir e fechar o accordion
 			$scope.problemas.forEach((problema, indice) => {
@@ -111,14 +152,17 @@ if ($autorizado) {
 		<script type="text/ng-template" id="problema.html">		
 		</script>
 		
-		<div class="container abas-container" ng-show="!carregandoProblemas">
+		<div class="container abas-container">
 			<uib-tabset active="tabAtivaProblemas" type="pills">
-				<uib-tab index="$index + 1" ng-repeat="item in menuProblemas.items" heading="{{item.title}}" classes="{{item.classes}}">
+				<uib-tab index="$index + 1" select="resgatarNomes($index)" ng-repeat="item in menuProblemas.items" heading="{{item.title}}" classes="{{item.classes}}">
 					<hr>
 				</uib-tab>
 			</uib-tabset>
+			<div id="alerta-carregamento" ng-show="carregandoProblemas">
+				Carregando problemas...
+			</div>
 			<uib-accordion close-others="true">
-				<div uib-accordion-group is-open="problema.aberto" class="panel-default" close-others="true" ng-repeat="problema in problemas | filter: {resolvido: tabAtivaProblemas - 1}">
+				<div uib-accordion-group is-open="problema.aberto" class="panel-default" close-others="true" ng-repeat="problema in problemas | filter: {resolvido: tabAtivaProblemas - 1}" ng-show="!carregandoProblemas">
 					<uib-accordion-heading>
 						<span>#{{problema.id}} - Indicador "{{problema.nome_indicador}}"</span><br><small class="novo-problema" ng-show="problema.novo == 1">Novo problema</small>
 					</uib-accordion-heading>
