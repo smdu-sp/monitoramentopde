3b42d39a8a8cfffede67c3959771325716058ba7
vgiuliani11 <vinicius.giuliani@gmail.com>
2025-05-15 23:03:13 -0300

    feat: novo formato de Ações Prioritárias, nova lógica de filtragem somente no front end (fix #76)

--- src/monitoramento_pde/templates/content-acoes-prioritarias.php
+++ src/monitoramento_pde/templates/content-acoes-prioritarias.php
@@ -1,5 +1,4 @@
-
-<script type="text/javascript">
+<script>
 jQuery.noConflict();
 
 var app = angular.module('monitoramentoPde', ['ngResource','ngAnimate','ui.bootstrap','angular.filter']);
@@ -9,128 +8,190 @@ app.factory('AcaoPrioritaria', function($resource){
 });
 
 app.controller("acoesPrioritarias", function($scope, $http, $filter, AcaoPrioritaria) {
+	$scope.todosOsCampos = ['politica_setorial', 'sistemas_urbanos_e_ambientais'];
+	$scope.filtros = {};
+	$scope.opcoes = {};
+	$scope.selectDesabilitado = {};
+	$scope.todosOsCampos.forEach(campo => {
+		$scope.filtros[campo] = null;
+		$scope.opcoes[campo] = [];
+		$scope.selectDesabilitado[campo] = false;
+	});
 	
-	 AcaoPrioritaria.query(function(acoesPrioritarias){
-		$scope.acoesPrioritarias = acoesPrioritarias;
-		$scope.acoesFiltro = $scope.acoesPrioritarias;
-	 });
-	
-	$scope.optCategoria = null;
-	$scope.optEstrategia = null;
-	$scope.optAndamento = null;
-	$scope.optArtigo = null;
-	$scope.optTema = null;
-	$scope.optEstagioImplementacao = null;
-	
-	$scope.cargaAcoesPrioritarias = function(){
-		
-		filtroCategoria = !$scope.optCategoria? null : $scope.optCategoria.categoria;
-		filtroEstrategia = !$scope.optEstrategia? null : $scope.optEstrategia.objetivo_relacionado;
-		filtroAndamento = !$scope.optAndamento? null : $scope.optAndamento.andamento;
-		filtroArtigo = !$scope.optArtigo? null : $scope.optArtigo.artigo;
-		filtroTema = !$scope.optTema? null : $scope.optTema.tema;
-		filtroEstagioImplementacao = !$scope.optEstagioImplementacao? null : $scope.optEstagioImplementacao.estagio_implementacao;
-		
-		AcaoPrioritaria.query({categoria:filtroCategoria, estrategia:filtroEstrategia, andamento:filtroAndamento, artigo:filtroArtigo, tema:filtroTema, estagio_implementacao: filtroEstagioImplementacao},function(acoesPrioritarias){
-		 $scope.acoesFiltro = acoesPrioritarias;
+	AcaoPrioritaria.query((acoes) => {
+		$scope.acoesPrioritarias = acoes;
+		$scope.acoesFiltro = acoes;
+
+		// Inicializar com todas as opções disponíveis
+		$scope.todosOsCampos.forEach(campo => {
+			$scope.opcoes[campo] = $filter('unique')(acoes, campo).map((obj => obj[campo]));
 		});
+	});
+
+	$scope.atualizarFiltros = (campoAtual) => {
+		const valorAtual = $scope.filtros[campoAtual];
+
+		// Guardar o primeiro filtro utilizado
+		if (!$scope.primeiroFiltro && valorAtual) {
+			$scope.primeiroFiltro = campoAtual;
+		}
+
+		// Ao limpar primeiro filtro, limpar os demais filtros
+		if (!$scope.filtros[$scope.primeiroFiltro]) {
+			$scope.primeiroFiltro = null;
+
+			$scope.todosOsCampos.forEach(campo => {
+				$scope.selectDesabilitado[campo] = false;
+				$scope.opcoes[campo] = $filter('unique')($scope.acoesPrioritarias, campo).map((obj => obj[campo]));
+			});
+
+			for (const filtro of Object.keys($scope.filtros)) {
+				$scope.filtros[filtro] = null;
+			}
+		}
+
+
+		// Atualiza os selects para mostrar apenas as opções disponíveis com os filtros aplicados
+		$scope.todosOsCampos.forEach(campo => {
+			// Não atualiza as opções do select se ele for primeiroFiltro
+			if (campo !== $scope.primeiroFiltro) {
+				let baseFiltrada = $scope.acoesPrioritarias;
+				
+				for (const [outroCampo, valor] of Object.entries($scope.filtros)) {
+					if (outroCampo !== campo && valor) {
+						baseFiltrada = baseFiltrada.filter(acao => acao[outroCampo] === valor);
+					}
+				}
+
+				const filtroOpcoes = $filter('unique')(baseFiltrada, campo).map((obj => obj[campo]));
+				$scope.opcoes[campo] = filtroOpcoes;
+
+				// Se restar só uma opção, selecionar automaticamente e desabilitar campo
+				if (filtroOpcoes.length === 1) {
+					const unicoValor = filtroOpcoes[0];
+
+					if ($scope.filtros[campo] !== unicoValor) {
+						$scope.filtros[campo] = unicoValor;
+					}
+
+					// Só desabilitar se não for o primeiro filtro
+					$scope.selectDesabilitado[campo] = ($scope.primeiroFiltro !== campo);
+				} else {
+					$scope.selectDesabilitado[campo] = false;
+				}
+			}
+		});
+
+		// Bloquear o campo do primeiroFiltro se mais de um filtro tiver sido aplicado
+		if ($scope.primeiroFiltro != campoAtual && valorAtual) {
+			$scope.selectDesabilitado[$scope.primeiroFiltro] = true;
+		} else {
+			$scope.selectDesabilitado[$scope.primeiroFiltro] = false;
+		}
+
+		// Aplicar todos os filtros
+		let acoesFiltradas = $scope.acoesPrioritarias;
+		
+		for (const [campo, valor] of Object.entries($scope.filtros)) {
+			if (valor) {
+				acoesFiltradas = acoesFiltradas.filter(acao => acao[campo] === valor);
+			}
+		}
+		
+		$scope.acoesFiltro = acoesFiltradas;
 	};
-	
 });
-
-
-
 </script>
 
-<div id="conteudo" class="content-page container text-justify" data-ng-app="monitoramentoPde" data-ng-controller="acoesPrioritarias">
-<?php the_content(); ?>
-
-<div class="row" style="margin:0;">
+<div
+	id="conteudo"
+	class="content-page container text-justify"
+	data-ng-app="monitoramentoPde"
+	data-ng-controller="acoesPrioritarias">
 	
-	<hr>
-	<div class="row">
+	<?php the_content(); ?>
+
+	<div class="row" style="margin:0;">	
+		<hr>
 		<div class="row">
-			<label for="estrategia" class="col-sm-12 acao-prioritaria" style="display:inline-block;float:none;vertical-align:middle;"> 
-				<strong> Estrategia </strong>
-			</label>
-		</div>
-		<div class="row">
-			<div class="col-sm-6 text-center" class="acao-prioritaria" style="display:inline-block;float:none;vertical-align:middle;"> 
-				<select style="width:100%;" data-ng-model="optEstrategia" class="acao-prioritaria" data-ng-options="acao.objetivo_relacionado for acao in acoesPrioritarias | unique: 'objetivo_relacionado' | filter: {objetivo_relacionado:''}" data-ng-change="cargaAcoesPrioritarias()" name="estrategia" id="estrategia">
-				<option value="">Todas</option>
-				</select>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-5" style="display:inline-block;float:none;">
-			<div class="col-sm-1 text-center" style="display:inline-block;float:none;vertical-align:middle;"> 
-				<!--<select style="max-width:100%;" data-ng-model="optEstrategia" data-ng-options="acao.estrategia for acao in acoesPrioritarias | unique: 'estrategia' | filter: {estrategia:''}" data-ng-change="cargaAcoesPrioritarias()" name="estrategia">
-				<option value="">Todos</option>
-				</select>-->
-			</div>
-				<div style="padding-left:15px; font-size:18px; margin-top: 10px; margin-bottom: 10px;">
-					<strong> Ação </strong>
+			<div class="row" style="font-size: 0">
+				<div class="col-sm-5" style="display:inline-block;float:none;">
+					<div style="padding-left:15px; font-size:18px; margin-top: 10px; margin-bottom: 10px;">
+						<strong> Ação Prioritária </strong>
+					</div>
 				</div>
-			</div><!--
-			--><div class="col-sm-1 acao-prioritaria" style="display:inline-block;float:none;vertical-align:middle;"> 
-				<strong> Artigo </strong>
-			</div><!--
-			--><div for="estagio_implementacao" class="col-sm-2 acao-prioritaria" style="display:inline-block;float:none;vertical-align:middle;padding-left:7px"> 
-				<strong> Estágio de implementação </strong>
-			</div><!--
-			--><label for="tema" class="col-sm-2 acao-prioritaria" style="display:inline-block;float:none;vertical-align:middle;padding-left:7px"> 
-				<strong> Tema </strong>
-			</label><!--
-			--><label for="categoria" class="col-sm-2 acao-prioritaria" style="display:inline-block;float:none;vertical-align:middle;padding-left:0"> 
-				<strong> Categoria </strong>
-			</label>
-		</div>
-		
-		<div class="row">
-			<div class="col-sm-5" style="display:inline-block;float:none;">
-			</div><!--
-			--><div class="col-sm-1" style="display:inline-block;float:none;">
-			</div><!--
-			--><div class="col-sm-2 text-center" style="display:inline-block;float:none;vertical-align:middle;padding-right:0px;">
-				<!-- TODO: DEFINIR LÓGICA PARA NÃO EXIBIR O COMBOBOX CASO NÃO EXISTA OPÇÃO A SER SELECIONADA -->
-				<!-- <select style="width:100%;" data-ng-model="optEstagio_implementacao" data-ng-options="acao.estagio_implementacao for acao in acoesPrioritarias | unique: 'estagio_implementacao' | filter: {estagio_implementacao:''}" data-ng-change="cargaAcoesPrioritarias()" name="estagio_implementacao" id="estagio_implementacao">
-				<option value="">Todos</option>
-				</select> -->
-			</div><!--
-			--><div class="col-sm-2 text-center" class="acao-prioritaria-cbo" style="display:inline-block;float:none;vertical-align:middle;padding-left:7px"> 
-				<select style="width:100%;" data-ng-model="optTema" class="acao-prioritaria" data-ng-options="acao.tema for acao in acoesPrioritarias | unique: 'tema' | filter: {tema:''}" data-ng-change="cargaAcoesPrioritarias()" name="tema" id="tema">
-				<option value="">Todos</option>
-				</select>
-			</div><!--
-			--><div class="col-sm-2 text-center" class="acao-prioritaria-cbo" style="display:inline-block;float:none;vertical-align:middle;padding-left:0"> 
-				<select style="width:100%;" data-ng-model="optCategoria" data-ng-options="acao.categoria for acao in acoesPrioritarias | unique: 'categoria' | filter: {categoria:''}" data-ng-change="cargaAcoesPrioritarias()" name="categoria" id="categoria">
-				<option value="">Todos</option>
-				</select>
-			</div>
-			
-			<!--<div class="col-sm-1 text-center" style="display:inline-block;float:none;vertical-align:middle;padding-right:30px;"> 
-				<select style="max-width:100%;" data-ng-model="optAndamento" data-ng-options="acao.andamento for acao in acoesPrioritarias | unique: 'andamento' | filter: {andamento:''}" data-ng-change="cargaAcoesPrioritarias()" name="andamento">
-					<option value="">Todos</option>
+				<div class="col-sm-1 acao-prioritaria" style="display:inline-block;float:none;vertical-align:middle;"> 
+					<strong>Artigo</strong>
+				</div>
+				<label
+					for="politica_setorial"
+					class="col-sm-3 acao-prioritaria"
+					style="
+						display:inline-block;
+						float:none;
+						vertical-align:middle;
+						padding-left:7px">
+					<strong>Política Setorial</strong>
+				</label>
+				<label
+					for="sistemas_urbanos_e_ambientais"
+					class="col-sm-3 acao-prioritaria"
+					style="
+						display:inline-block;
+						float:none;
+						vertical-align:middle;
+						padding-left:7px"> 
+					<strong>Sistemas Urbanos e Ambientais</strong>
+				</label>
+			</div>			
+			<div class="row" style="font-size: 0">
+				<div class="col-sm-5" style="display:inline-block;float:none;">
+				</div>
+				<div class="col-sm-1" style="display:inline-block;float:none;">
+				</div>
+				<div
+					class="col-sm-3 text-center acao-prioritaria-cbo"
+					style="
+						display:inline-block;
+						float:none;
+						vertical-align:middle;
+						padding-left:7px">
+					<select
+						style="width: 100%"
+						id="politica_setorial"
+						ng-model="filtros.politica_setorial"
+						ng-options="politica for politica in opcoes.politica_setorial"
+						ng-change="atualizarFiltros('politica_setorial')"
+						ng-disabled="selectDesabilitado.politica_setorial">
+						<option value="">Todos</option>
 					</select>
-			</div>-->
-		</div>
-		
-		<ul class="list-group">
-			<li class="list-group-item row list-pontilhada" data-ng-repeat="acao in acoesFiltro | orderBy: 'artigo'">
-				<div class="col-sm-5 acao-prioritaria"> {{acao.acao_prioritaria_estrategica}} </div>
-				<div class="col-sm-1 acao-prioritaria"> <small>{{acao.artigo}}</small> </div>
-				<div class="col-sm-2 acao-prioritaria"> {{acao.estagio_implementacao}} </div>
-				<div class="col-sm-2 acao-prioritaria"> {{acao.tema}} </div>
-				<div class="col-sm-2 acao-prioritaria"> {{acao.categoria}} </div>
-				<!--<div class="col-sm-1 acao-prioritaria text-center"> {{acao.andamento}}</div>-->
-			</li>
-		</ul>
+				</div>
+				<div
+					class="col-sm-3 text-center acao-prioritaria-cbo"
+					style="display:inline-block;float:none;vertical-align:middle;padding-left:0"> 
+					<select
+						style="width: 100%"
+						id="sistemas_urbanos_e_ambientais"
+						ng-model="filtros.sistemas_urbanos_e_ambientais"
+						ng-options="sistema for sistema in opcoes.sistemas_urbanos_e_ambientais"
+						ng-change="atualizarFiltros('sistemas_urbanos_e_ambientais')"
+						ng-disabled="selectDesabilitado.sistemas_urbanos_e_ambientais">
+						<option value="">Todos</option>
+					</select>
+				</div>
+			</div>			
+			<ul class="list-group">
+				<li
+					class="list-group-item row list-pontilhada"
+					data-ng-repeat="acao in acoesFiltro | orderBy: 'artigo'">
+					<div class="col-sm-5 acao-prioritaria"> {{acao.acao_prioritaria }} </div>
+					<div class="col-sm-1 acao-prioritaria"> <small>{{acao.artigo}}</small> </div>
+					<div class="col-sm-3 acao-prioritaria"> {{acao.politica_setorial}} </div>
+					<div class="col-sm-3 acao-prioritaria"> {{acao.sistemas_urbanos_e_ambientais}} </div>
+				</li>
+			</ul>
+		</div>	
 	</div>
-	
 </div>
-
-
-</div>
-
 
 <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
